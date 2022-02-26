345431f25d24f0a534b64470ed97b11ca1ce5f41
LÊ NGỌC VI <you@example.com>
2022-02-26 17:21:38 +0700

    Dinh nghia cac route cho tai nguyen Contact

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
 const express = require("express");
 const cors = require("cors");
-
+const setupContactRoutes = require("./app/routes/contact.routes");
 const app = express();
- 
+
+const PORT = process.env.PORT || 8080;
+
+const {BadRequestError, errorHandler} = require("./app/errors");
+//const app = express();
 app.use(cors());
 
 app.use(express.json());
@@ -13,4 +17,16 @@ app.get("/", (req, res) => {
     res.json({ message: "Welcome to contac book application."});
 });
 
+setupContactRoutes(app);
+app.use((req, res, next) => {
+    next(new BadRequestError(404, "Resourve not found"));
+});
+
+app.use((err, req, res, next) => {
+    errorHandler.handleError(erros, res);
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is runging on port &{PORT}`);
+});
 module.exports = app;
