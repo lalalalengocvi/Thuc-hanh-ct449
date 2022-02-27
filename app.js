2bd487f642c8b071810b702cbf93e92b280ebf00
LÊ NGỌC VI <you@example.com>
2022-02-27 18:05:54 +0700

    Cai dat experss chay thong bao xin chao

--- app.js
+++ app.js
@@ -1,32 +1,32 @@
 const express = require("express");
 const cors = require("cors");
-const setupContactRoutes = require("./app/routes/contact.routes");
+//const setupContactRoutes = require("./app/routes/contact.routes");
 const app = express();
 
-const PORT = process.env.PORT || 8080;
+//const PORT = process.env.PORT || 8080;
 
-const {BadRequestError, errorHandler} = require("./app/errors");
+//const {BadRequestError, errorHandler} = require("./app/errors");
 //const app = express();
 app.use(cors());
 
 app.use(express.json());
 
-app.use(express.urlencoded({ extended: true}));
+//app.use(express.urlencoded({ extended: true}));
 
 app.get("/", (req, res) => {
-    res.json({ message: "Welcome to contac book application."});
+    res.json({ message: "Welcome to contac book application." });
 });
 
-setupContactRoutes(app);
-app.use((req, res, next) => {
-    next(new BadRequestError(404, "Resourve not found"));
-});
+//setupContactRoutes(app);
+//app.use((req, res, next) => {
+  //  next(new BadRequestError(404, "Resourve not found"));
+//});
 
-app.use((err, req, res, next) => {
-    errorHandler.handleError(erros, res);
-});
+//app.use((err, req, res, next) => {
+  //  errorHandler.handleError(erros, res);
+//});
 
-app.listen(PORT, () => {
-    console.log(`Server is runging on port &{PORT}`);
-});
+//app.listen(PORT, () => {
+   // console.log(`Server is runging on port &{PORT}`);
+//});
 module.exports = app;
